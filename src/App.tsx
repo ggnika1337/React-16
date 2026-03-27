1ee2dad41e6a88148dbfb4a41cfd206bb97c6083
ggnika1337 <nikushanika568@gmail.com>
2026-03-27 15:52:55 +0400

    First Commit: images filters and responsive left

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import "./App.css";
+import Job from "./components/__organisms/Job/Job";
+import data from "../data.json";
+import { useState } from "react";
+
+function App() {
+  const [isFilter, setIsFilter] = useState(true);
+  const filters: array = [];
+  console.log(filters);
+  return (
+    <>
+      <div className="background w-full h-[156px] z-1"></div>
+
+      <div className="container flex flex-col z-2 pt-[210px] gap-[40px] max-w-[1110px] relative">
+        <div
+          style={{
+            display: isFilter ? "flex" : "none",
+          }}
+          className="px-[40px] max-w-[1110px] top-[100px] absolute py-[20px] w-full h-[72px] flex justify-between items-center bg-white shadow-[0px_15px_20px_-5px_#0D718226] rounded-[5px]"
+        >
+          <div>
+            {filters.map(() => {
+              return;
+            })}
+          </div>
+          <h1
+            onClick={() => {
+              filters.length = 0;
+              console.log(filters);
+            }}
+            className="font-[800] text-[20px] leading-[24px] text-[#5CA5A5] cursor-pointer"
+          >
+            Clear
+          </h1>
+        </div>
+        {data.map((el, key) => {
+          return (
+            <div key={key}>
+              <Job
+                img={el.logo}
+                company={el.company}
+                position={el.position}
+                contract={el.contract}
+                location={el.location}
+                postedAt={el.postedAt}
+                languages={el.languages}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/__organisms/Job/Job.tsx
@@ -0,0 +1,69 @@
+function Job({
+  img,
+  isNew,
+  company,
+  position,
+  postedAt,
+  contract,
+  location,
+  languages,
+}) {
+  return (
+    <>
+      <div
+        style={{
+          borderLeft: isNew ? "5px solid #5CA5A5" : "none",
+        }}
+        className="w-full h-[152px] bg-white flex justify-between p-[32px] shadow-[0px_15px_20px_-5px_#0D718226] rounded-[5px]"
+      >
+        <div className="flex">
+          <div className="flex gap-[30px]">
+            <img src={img} className="size-[88px] rounded-full" />
+            <div className="flex flex-col gap-[5px]">
+              <h1 className="text-[#5CA5A5] font-[700] text-[18px]">
+                {company}
+              </h1>
+              <div
+                style={{
+                  display: isNew ? "flex" : "none",
+                }}
+              >
+                <div className="flex gap-[8px]">
+                  <div className="px-[8px] py-[5px] bg-[#5CA5A5]">
+                    <h1 className="text-white font-[700] text-[14px]">NEW!</h1>
+                  </div>
+                  <div className="bg-[#2B3939] px-[8px] py-[5px]">
+                    <h1 className="text-white font-[700] text-[14px]">
+                      FEATURED
+                    </h1>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <span className="text-[#5CA5A5] font-[700] text-[22px] cursor-pointer">
+                  {position}
+                </span>
+                <div className="flex gap-[18px] text-[#7C8F8F] font-[500]">
+                  <span>{postedAt}</span>
+                  <h1>•</h1>
+                  <span>{contract}</span>
+                  <h1>•</h1>
+                  <span>{location}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-[16px]">
+          <div className="px-[10px] py-[5px] bg-[#5ca5a59d] hover:bg-[#5ca5a5]">
+            <h1 className="text-white font-[700] cursor-pointer">
+              {languages}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Job;
