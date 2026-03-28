2e0ce44f3ec4b82c4bb844b2eab7d978288a4806
ggnika1337 <nikushanika568@gmail.com>
2026-03-29 01:33:18 +0400

    Fix: tags fixed (now displaying text instead of 1,2,3,4...)

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,61 @@ import "./App.css";
 import Job from "./components/__organisms/Job/Job";
 import data from "../data.json";
 import { useState } from "react";
+import X from "./assets/images/X.svg";
 
 function App() {
   const [isFilter, _setIsFilter] = useState(false);
-  const filters: string[] = [];
+  const [filters, setFilters] = useState([]);
+
+  function handleFilters(e: React.MouseEvent<HTMLDivElement>) {
+    _setIsFilter(true);
+    let isInArray: boolean = false;
+    // check if array already has said filter
+    if (filters.includes(e.currentTarget.textContent)) {
+      isInArray = true;
+    }
+    // if array doesnt have filter, add
+    if (isInArray !== true) {
+      setFilters([...filters, e.currentTarget.textContent]);
+      console.log(filters);
+    }
+  }
+
   return (
     <>
       <div className="background w-full h-[156px] z-1"></div>
 
-      <div className="container flex flex-col z-2 pt-[210px] gap-[40px] max-w-[1110px] relative">
+      <div className="container flex flex-col z-3 pt-[210px] gap-[40px] max-w-[1110px] relative">
         <div
           style={{
             display: isFilter ? "flex" : "none",
           }}
-          className="px-[40px] max-w-[1110px] top-[100px] absolute py-[20px] w-full h-[72px] flex justify-between items-center bg-white shadow-[0px_15px_20px_-5px_#0D718226] rounded-[5px]"
+          className="px-[40px] max-md:px-[20px] max-md:py-[20px] max-w-[1110px] top-[100px] absolute py-[20px] w-full flex justify-between items-center bg-white shadow-[0px_15px_20px_-5px_#0D718226] rounded-[5px] h-auto"
         >
-          <div>
-            {filters.map(() => {
-              return "123";
+          <div className="w-full h-[32px] flex gap-[16px] flex-wrap">
+            {filters.map((text: string, key: number) => {
+              return (
+                <div
+                  className="flex items-end bg-[#5ca5a52d] pl-[10px] rounded-[10px] gap-[11px]"
+                  key={key}
+                >
+                  <h1 className="text-[#5CA5A5] font-[700] text-[16px] mb-[3px]">
+                    {text}
+                  </h1>
+                  <div className="w-[32px] h-full flex justify-center rounded-r-[10px] items-center bg-[#5CA5A5] hover:bg-[#2B3939]">
+                    <img src={X} />
+                  </div>
+                </div>
+              );
             })}
           </div>
           <h1
             onClick={() => {
               filters.length = 0;
+              _setIsFilter(false);
               console.log(filters);
             }}
-            className="font-[800] text-[20px] leading-[24px] text-[#5CA5A5] cursor-pointer"
+            className="font-[800] text-[20px] leading-[24px] hover:text-[#5CA5A5] text-[#7C8F8F] cursor-pointer"
           >
             Clear
           </h1>
@@ -36,13 +65,7 @@ function App() {
           return (
             <div key={key}>
               <Job
-                onClick={() => {
-                  if (isFilter === true) {
-                    _setIsFilter(false);
-                  } else {
-                    _setIsFilter(true);
-                  }
-                }}
+                onClick={handleFilters}
                 img={el.logo}
                 company={el.company}
                 position={el.position}
